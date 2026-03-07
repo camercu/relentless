b121be026394d3d3c63e624d5a17810b372acc0a
camercu <cameron.unterberger@gmail.com>
2026-03-07 07:34:35 -0600

    chore(devx): tighten pre-commit and shell tooling

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
 let
   pinned_nixpkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/ed142ab1b3a092c4d149245d0c4126a5d7ea00b0.tar.gz";
+    sha256 = "1h7v295lpjfxpxkag2csam7whx918sdypixdi8i85vlb707gg0vm";
   };
   pkgs = import pinned_nixpkgs {};
 in
@@ -8,6 +9,7 @@ pkgs.mkShell {
   packages = with pkgs; [
     rustup
     just
+    pre-commit
     cargo-deny
     typos
     taplo
